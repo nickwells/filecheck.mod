fcfb2f8937712eb8d60d8608ad354f0472ec49ab
Nick Wells <wells.nicholas@gmail.com>
2020-07-03 10:46:43 +0100

    Add some functions to return commonly useful Provisos

---
+++ filecheck/standardProvisos.go
@@ -0,0 +1,43 @@
+package filecheck
+
+import "github.com/nickwells/check.mod/check"
+
+// DirExists returns a Provisos that will check that the value refers
+// to a directory, which must exist. This is a common Provisos value and this
+// func is provided to simplify your code.
+func DirExists() Provisos {
+	return Provisos{
+		Existence: MustExist,
+		Checks:    []check.FileInfo{check.FileInfoIsDir},
+	}
+}
+
+// FileExists returns a Provisos that will check that the value refers to a
+// regular file, which must exist. This is a common Provisos value and this
+// func is provided to simplify your code.
+func FileExists() Provisos {
+	return Provisos{
+		Existence: MustExist,
+		Checks:    []check.FileInfo{check.FileInfoIsRegular},
+	}
+}
+
+// FileNonEmpty returns a Provisos that will check that the value refers to a
+// regular file, which must exist and must not be empty. This is a common
+// Provisos value and this func is provided to simplify your code.
+func FileNonEmpty() Provisos {
+	return Provisos{
+		Existence: MustExist,
+		Checks: []check.FileInfo{
+			check.FileInfoIsRegular,
+			check.FileInfoSize(check.Int64GT(0)),
+		},
+	}
+}
+
+// IsNew returns a Provisos that will check that the value refers to a
+// non-existant file/directory. This is a common Provisos value and this func
+// is provided to simplify your code.
+func IsNew() Provisos {
+	return Provisos{Existence: MustNotExist}
+}
