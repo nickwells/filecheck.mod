e8977be22394a618f1bf9da23aea5521692c7b8a
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- filecheck/doc.go
+++ filecheck/doc.go
@@ -1,10 +1,8 @@
 /*
-
 Package filecheck provides a means of checking that certain constraints apply
 to a file-system object. There are checks on the existence of the file and on
 it's status. The caller can opt to apply the checks to the object itself or,
 if it is a symbolic link, to the object being linked to. The default
 behaviour will check the status of the linked-to object.
-
 */
 package filecheck
